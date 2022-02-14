201b57809a0413182fa331cc41a850a61c7d656c
Clayvis Rawlins <Clayvis27@gmail.com>
2022-02-14 11:24:02 -0800

    Changed File Structures and added routing

--- sports-app/src/App.js
+++ sports-app/src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <h1>Whats goin on</h1>;
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path={"/"} element={<Home />}></Route>
+					{/* <Route path={"/signin"} element={<SignInSide />}></Route> */}
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
