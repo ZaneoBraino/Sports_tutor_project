6c596872ef3b134ad7afc969ffab3f76c6a48575
ZaneoBraino <97763737+ZaneoBraino@users.noreply.github.com>
2022-02-14 15:04:05 -0800

    Added header/footer and imported styles

--- sports-app/src/App.js
+++ sports-app/src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
 
 function App() {
-	return (
-		<div>
-			<Router>
-				<Routes>
-					<Route path={"/"} element={<Home />}></Route>
-					{/* <Route path={"/signin"} element={<SignInSide />}></Route> */}
-				</Routes>
-			</Router>
-		</div>
-	);
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path={"/"} element={<Home />}></Route>
+          {/* <Route path={"/signin"} element={<SignInSide />}></Route> */}
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
