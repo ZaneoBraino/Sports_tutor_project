92891afbf3b1d1dcda0d121a104d49a28fd9d4e1
slichy6 <94019317+slichy6@users.noreply.github.com>
2022-02-15 17:26:19 -0600

    coaches pg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
 import About from "./pages/About";
+import coaches from "./pages/coaches";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path={"/"} element={<Home />}></Route>
           <Route path={"/about"} element={<About />}></Route>
+          <Route path={"/coaches"} element={<coaches />}></Route>
           {/* <Route path={"/signin"} element={<SignInSide />}></Route> */}
         </Routes>
         <Footer />
---
+++ frontend/src/pages/Coaches.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+function coaches(){
+    return (
+        <div class="card">
+  <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhZHNob3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" class="card-img-top" class="card-img-top" alt="Fissure in Sandstone"/>
+  <div class="card-body">
+    <h5 class="card-title">Coach Clay</h5>
+    <p class="card-text">Located: Daytona, Florida</p>
+    <div class="progress">
+  <div
+    class="progress-bar bg-success"
+    role="progressbar"
+    style="width: 25%;"
+    aria-valuenow="25"
+    aria-valuemin="0"
+    aria-valuemax="100"
+  ></div>
+</div>
+<div class="progress">
+  <div
+    class="progress-bar bg-info"
+    role="progressbar"
+    style="width: 50%;"
+    aria-valuenow="50"
+    aria-valuemin="0"
+    aria-valuemax="100"
+  ></div>
+</div>
+<div class="progress">
+  <div
+    class="progress-bar bg-warning"
+    role="progressbar"
+    style="width: 75%;"
+    aria-valuenow="75"
+    aria-valuemin="0"
+    aria-valuemax="100"
+  ></div>
+</div>
+<div class="progress">
+  <div
+    class="progress-bar bg-danger"
+    role="progressbar"
+    style="width: 100%;"
+    aria-valuenow="100"
+    aria-valuemin="0"
+    aria-valuemax="100"
+  ></div>
+</div>
+    <a href="#!" class="btn btn-primary">Contact</a>
+  </div>
+</div>
+    )
+}
+export default coaches;
