5fdb76de85bde5c4bf8c4d3f84cfaeef93f6b300
ZaneoBraino <97763737+ZaneoBraino@users.noreply.github.com>
2022-02-15 11:12:31 -0800

    Just pushing progress for the landing

---
+++ frontend/src/pages/Landing_page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Landing_page() {
+  return (
+    <div class="container">
+      <img class="cover" src="img/cover.jpg"></img>
+      <div class="coverText">
+        <h1></h1>
+      </div>
+    </div>
+  );
+}
+
+export default Landing_page;
