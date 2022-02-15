727c981c6a96bd8fe438c92a6e5f53d81fed4c8a
slichy6 <94019317+slichy6@users.noreply.github.com>
2022-02-15 15:39:19 -0600

    about page and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path={"/"} element={<Home />}></Route>
+          <Route path={"/about"} element={<About />}></Route>
           {/* <Route path={"/signin"} element={<SignInSide />}></Route> */}
         </Routes>
         <Footer />
