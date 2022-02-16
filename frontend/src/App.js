a8cc87c6b8b5171309fcad489f54139dda1fcfd2
ZaneoBraino <97763737+ZaneoBraino@users.noreply.github.com>
2022-02-16 08:18:33 -0800

    Landing page part 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Home from "./pages/Home";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
-import Landing_page from "./pages/Landingpage";
+import Landing_page from "./pages/Landing_page";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           {/* <Route path={"/"} element={<Home />}></Route> */}
           {/* <Route path={"/signin"} element={<SignInSide />}></Route> */}
-          <Route path={"/landingpage"} element={<Landing_page />}></Route>
+          <Route path={"/Landing_page"} element={<Landing_page />}></Route>
         </Routes>
         <Footer />
       </Router>
