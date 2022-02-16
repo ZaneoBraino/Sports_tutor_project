706ed9de389953c243556e7eb167df83cbe488cc
slichy6 <94019317+slichy6@users.noreply.github.com>
2022-02-15 22:20:31 -0600

    add cards

--- frontend/src/pages/Coaches.js
+++ frontend/src/pages/Coaches.js
@@ -1,55 +1,46 @@
 import React from "react";
 
-function Coaches(){
+function coaches(){
     return (
         <div class="card">
-  <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhZHNob3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" class="card-img-top" class="card-img-top" alt="Fissure in Sandstone"/>
-  <div class="card-body">
-    <h5 class="card-title">Coach Clay</h5>
-    <p class="card-text">Located: Daytona, Florida</p>
-    <div class="progress">
-  <div
-    class="progress-bar bg-success"
-    role="progressbar"
-    style="width: 25%;"
-    aria-valuenow="25"
-    aria-valuemin="0"
-    aria-valuemax="100"
-  ></div>
-</div>
-<div class="progress">
-  <div
-    class="progress-bar bg-info"
-    role="progressbar"
-    style="width: 50%;"
-    aria-valuenow="50"
-    aria-valuemin="0"
-    aria-valuemax="100"
-  ></div>
-</div>
-<div class="progress">
-  <div
-    class="progress-bar bg-warning"
-    role="progressbar"
-    style="width: 75%;"
-    aria-valuenow="75"
-    aria-valuemin="0"
-    aria-valuemax="100"
-  ></div>
-</div>
-<div class="progress">
-  <div
-    class="progress-bar bg-danger"
-    role="progressbar"
-    style="width: 100%;"
-    aria-valuenow="100"
-    aria-valuemin="0"
-    aria-valuemax="100"
-  ></div>
-</div>
-    <a href="#!" class="btn btn-primary">Contact</a>
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img
+        src="https://mdbcdn.b-cdn.net/wp-content/uploads/2020/06/vertical.webp"
+        alt="Trendy Pants and Shoes"
+        class="img-fluid rounded-start"
+      />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Coach Clay</h5>
+        <p class="card-text">
+          Located: Daytona Beach, FL
+        </p>
+        <p class="card-text">
+          <small class="text-muted">A little about me.....</small>
+        </p>
+      </div>
+    </div>
   </div>
 </div>
+//         <div class="card">
+//   <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" class="card-img-top" alt="Fissure in Sandstone"/>
+//   <div class="card-body">
+//     <h5 class="card-title">Coach Clay</h5>
+//     <p class="card-text">Location: Daytona, FL</p>
+//     <a href="#!" class="btn btn-primary">Contact</a>
+//   </div>
+//   <div class="card">
+//   <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" class="card-img-top" alt="Fissure in Sandstone"/>
+//   <div class="card-body">
+//     <h5 class="card-title">Coach Meri</h5>
+//     <p class="card-text">Location: Unknown</p>
+//     <a href="#!" class="btn btn-primary">Contact</a>
+//   </div>
+// </div>
+// </div>
+
     )
 }
 export default coaches;
