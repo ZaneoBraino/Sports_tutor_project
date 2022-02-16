cab3a7a96a58637a2ab4ad691578b9f8bba91181
Clayvis Rawlins <Clayvis27@gmail.com>
2022-02-16 08:46:56 -0800

    created database, register page, and login pages

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	connectionString:
+		"mongodb+srv://Clay:Dusty27@cluster0.t5ah1.mongodb.net/ClayWorld?retryWrites=true&w=majority",
+	secretOrKey: "secret",
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path={"/"} element={<Home />}></Route>
-          {/* <Route path={"/signin"} element={<SignInSide />}></Route> */}
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Header />
+				<Routes>
+					<Route path={"/"} element={<Home />}></Route>
+					<Route path={"/Register"} element={<Register />}></Route>
+					<Route path={"/Login"} element={<Login />}></Route>
+				</Routes>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
