7601e2d9f9da65547a37e1152e37059c378f2e12
Timothyolo <97767706+Timothyolo@users.noreply.github.com>
2022-02-16 07:16:04 -0600

    new

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,8 +1,58 @@
 import React from "react";
 import "./SignUp.css";
+import "./component./style.css";
 
 function CoachSignUp() {
-  return <h1>Testing</h1>;
+  return (
+    <div className="register-screen">
+      <form className="register-screen_form">
+        <h3 className="register-screen_title">Sign Up</h3>
+        <div className="form-group">
+          <label htmlFor="name">User Name</label>
+          <input
+            type="text"
+            required
+            id="name"
+            placeholder="Enter username"
+          ></input>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="name">Email</label>
+          <input
+            type="text"
+            required
+            id="email"
+            placeholder="Enter Email"
+          ></input>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="name">Password</label>
+          <input
+            type="text"
+            required
+            id="password"
+            placeholder="Enter Password"
+          ></input>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="name">Confirm Password</label>
+          <input
+            type="text"
+            required
+            id="confirm password"
+            placeholder="Enter confirm password"
+          ></input>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CoachSignUp;
